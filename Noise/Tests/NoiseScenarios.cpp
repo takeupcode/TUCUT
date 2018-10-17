04412bb5a6a416ce86a79a88af471c23c121902a
Wahid Tanner <wahidtanner@gmail.com>
2018-10-17 01:28:05 -0400

    Changed the min/max calc and added seed to filename.

--- Noise/Tests/NoiseScenarios.cpp
+++ Noise/Tests/NoiseScenarios.cpp
@@ -29,32 +29,32 @@ SCENARIO( Hash, "Construction/Normal", "unit,hash", "NoiseGenerator can be const
 SCENARIO( Hash, "Operation/Normal", "unit,hash", "NoiseGenerator can generate 2D noise." )
 {
     Noise::NoiseGenerator noise;
+    int seed = noise.seed();
     
     const uint32_t width = 512, height = 512;
     double *noiseMap = new double[width * height];
     size_t layers = 4;
 
+    double min = std::numeric_limits<double>::max();
+    double max = std::numeric_limits<double>::min();
     for (uint32_t y = 0; y < height; ++y)
     {
         for (uint32_t x = 0; x < width; ++x)
         {
-            noiseMap[y * width + x] = noise.generate(static_cast<double>(x) / 64, static_cast<double>(y) / 64, nullptr, layers);
+            double noiseValue = noise.generate(static_cast<double>(x) / 64, static_cast<double>(y) / 64, nullptr, layers);
+            noiseMap[y * width + x] = noiseValue;
+            
+            if (noiseValue < min)
+            {
+                min = noiseValue;
+            }
+            else if (noiseValue > max)
+            {
+                max = noiseValue;
+            }
         }
     }
     
-    double min = std::numeric_limits<double>::max();
-    double max = std::numeric_limits<double>::min();
-    for (unsigned i = 0; i < width * height; ++i)
-    {
-        if (noiseMap[i] < min)
-        {
-            min = noiseMap[i];
-        }
-        if (noiseMap[i] > max)
-        {
-            max = noiseMap[i];
-        }
-    }
     double minMaxShift = -min;
     double minMaxRange = max - min;
     if (minMaxRange == 0)
@@ -62,8 +62,11 @@ SCENARIO( Hash, "Operation/Normal", "unit,hash", "NoiseGenerator can generate 2D
         minMaxRange = 1;
     }
 
+    std::string fileName = "./noise";
+    fileName += std::to_string(seed);
+    fileName += ".ppm";
     std::ofstream ofs;
-    ofs.open("./noise.ppm", std::ios::out | std::ios::binary | std::ios::trunc);
+    ofs.open(fileName, std::ios::out | std::ios::binary | std::ios::trunc);
     ofs << "P6\n" << width << " " << height << "\n255\n";
     for (unsigned i = 0; i < width * height; ++i)
     {
