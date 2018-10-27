94f453fb6ebcd6f4630aea5b0dd42c6eeed6e97c
Wahid Tanner <wahidtanner@gmail.com>
2018-10-26 23:33:36 -0400

    Added more point and vector tests.

--- Math/Tests/PointScenarios.cpp
+++ Math/Tests/PointScenarios.cpp
@@ -15,87 +15,176 @@ using namespace TUCUT;
 
 SCENARIO( Point, "Construction/Normal", "unit,math", "Points can be constructed." )
 {
-    Math::Point1i pt1;
-    Math::Point2i pt2;
-    Math::Point3i pt3;
-    Math::Point4i pt4;
-    Math::Point1i pt5(123);
-    Math::Point2i pt6(123, 124);
-    Math::Point3i pt7(123, 124, 125);
-    Math::Point4i pt8(123, 124, 125, 126);
+    Math::Point1i pt1a;
+    Math::Point2i pt2a;
+    Math::Point3i pt3a;
+    Math::Point4i pt4a;
+    Math::Point1i pt1b(123);
+    Math::Point2i pt2b(123, 124);
+    Math::Point3i pt3b(123, 124, 125);
+    Math::Point4i pt4b(123, 124, 125, 126);
     
-    VERIFY_EQUAL(0, pt1.x);
+    VERIFY_EQUAL(0, pt1a.x);
 
-    VERIFY_EQUAL(0, pt2.x);
-    VERIFY_EQUAL(0, pt2.y);
+    VERIFY_EQUAL(0, pt2a.x);
+    VERIFY_EQUAL(0, pt2a.y);
 
-    VERIFY_EQUAL(0, pt3.x);
-    VERIFY_EQUAL(0, pt3.y);
-    VERIFY_EQUAL(0, pt3.z);
+    VERIFY_EQUAL(0, pt3a.x);
+    VERIFY_EQUAL(0, pt3a.y);
+    VERIFY_EQUAL(0, pt3a.z);
 
-    VERIFY_EQUAL(0, pt4.x);
-    VERIFY_EQUAL(0, pt4.y);
-    VERIFY_EQUAL(0, pt4.z);
-    VERIFY_EQUAL(0, pt4.w);
+    VERIFY_EQUAL(0, pt4a.x);
+    VERIFY_EQUAL(0, pt4a.y);
+    VERIFY_EQUAL(0, pt4a.z);
+    VERIFY_EQUAL(0, pt4a.w);
     
-    VERIFY_EQUAL(123, pt5.x);
+    VERIFY_EQUAL(123, pt1b.x);
     
-    VERIFY_EQUAL(123, pt6.x);
-    VERIFY_EQUAL(124, pt6.y);
+    VERIFY_EQUAL(123, pt2b.x);
+    VERIFY_EQUAL(124, pt2b.y);
     
-    VERIFY_EQUAL(123, pt7.x);
-    VERIFY_EQUAL(124, pt7.y);
-    VERIFY_EQUAL(125, pt7.z);
+    VERIFY_EQUAL(123, pt3b.x);
+    VERIFY_EQUAL(124, pt3b.y);
+    VERIFY_EQUAL(125, pt3b.z);
     
-    VERIFY_EQUAL(123, pt8.x);
-    VERIFY_EQUAL(124, pt8.y);
-    VERIFY_EQUAL(125, pt8.z);
-    VERIFY_EQUAL(126, pt8.w);
+    VERIFY_EQUAL(123, pt4b.x);
+    VERIFY_EQUAL(124, pt4b.y);
+    VERIFY_EQUAL(125, pt4b.z);
+    VERIFY_EQUAL(126, pt4b.w);
 }
 
 SCENARIO( Point, "Construction/Normal", "unit,math", "Points can be copy constructed." )
 {
-    Math::Point1i pt1(123);
-    Math::Point1i pt2(pt1);
+    Math::Point1i pt1a(123);
+    Math::Point1i pt1b(pt1a);
     
-    VERIFY_EQUAL(pt1, pt2);
+    VERIFY_EQUAL(pt1a, pt1b);
+    
+    Math::Point2i pt2a(123, 124);
+    Math::Point2i pt2b(pt2a);
+    
+    VERIFY_EQUAL(pt2a, pt2b);
+    
+    Math::Point3i pt3a(123, 124, 125);
+    Math::Point3i pt3b(pt3a);
+    
+    VERIFY_EQUAL(pt3a, pt3b);
+    
+    Math::Point4i pt4a(123, 124, 125, 126);
+    Math::Point4i pt4b(pt4a);
+    
+    VERIFY_EQUAL(pt4a, pt4b);
 }
 
 SCENARIO( Point, "Operation/Normal", "unit,math", "Points can be assigned." )
 {
-    Math::Point1i pt1(123);
-    Math::Point1i pt2(124);
+    Math::Point1i pt1a(123);
+    Math::Point1i pt1b(124);
     
-    VERIFY_FALSE(pt1 == pt2);
-    pt2 = pt1;
-    VERIFY_EQUAL(pt1, pt2);
+    VERIFY_FALSE(pt1a == pt1b);
+    pt1b = pt1a;
+    VERIFY_EQUAL(pt1a, pt1b);
+    
+    Math::Point2i pt2a(123, 124);
+    Math::Point2i pt2b(124, 125);
+    
+    VERIFY_FALSE(pt2a == pt2b);
+    pt2b = pt2a;
+    VERIFY_EQUAL(pt2a, pt2b);
+    
+    Math::Point3i pt3a(123, 124, 125);
+    Math::Point3i pt3b(124, 125, 126);
+    
+    VERIFY_FALSE(pt3a == pt3b);
+    pt3b = pt3a;
+    VERIFY_EQUAL(pt3a, pt3b);
+    
+    Math::Point4i pt4a(123, 124, 125, 126);
+    Math::Point4i pt4b(124, 125, 126, 127);
+    
+    VERIFY_FALSE(pt4a == pt4b);
+    pt4b = pt4a;
+    VERIFY_EQUAL(pt4a, pt4b);
 }
 
 SCENARIO( Point, "Operation/Normal", "unit,math", "Points can be subtracted and interact with vector." )
 {
-    Math::Point2i pt1(123, 123);
-    Math::Point2i pt2(124, 124);
+    Math::Point1i pt1a(123);
+    Math::Point1i pt1b(124);
+    Math::Vector1i vec1 = pt1b - pt1a;
+    VERIFY_EQUAL(1, vec1.x);
     
-    Math::Vector2i diff = pt2 - pt1;
+    Math::Point1i pt1c = pt1a + vec1;
+    VERIFY_EQUAL(pt1b, pt1c);
+    
+    Math::Point1i pt1d = pt1b - vec1;
+    VERIFY_EQUAL(pt1a, pt1d);
 
-    VERIFY_EQUAL(1, diff.x);
-    VERIFY_EQUAL(1, diff.y);
+    Math::Point2i pt2a(123, 123);
+    Math::Point2i pt2b(124, 124);
+    Math::Vector2i vec2 = pt2b - pt2a;
+    VERIFY_EQUAL(1, vec2.x);
+    VERIFY_EQUAL(1, vec2.y);
     
-    Math::Point2i pt3 = pt1 + diff;;
+    Math::Point2i pt2c = pt2a + vec2;
+    VERIFY_EQUAL(pt2b, pt2c);
+    
+    Math::Point2i pt2d = pt2b - vec2;
+    VERIFY_EQUAL(pt2a, pt2d);
 
-    VERIFY_EQUAL(pt2, pt3);
+    Math::Point3i pt3a(123, 123, 123);
+    Math::Point3i pt3b(124, 124, 124);
+    Math::Vector3i vec3 = pt3b - pt3a;
+    VERIFY_EQUAL(1, vec3.x);
+    VERIFY_EQUAL(1, vec3.y);
+    VERIFY_EQUAL(1, vec3.z);
+
+    Math::Point3i pt3c = pt3a + vec3;
+    VERIFY_EQUAL(pt3b, pt3c);
     
-    Math::Point2i pt4 = pt2 - diff;;
+    Math::Point3i pt3d = pt3b - vec3;
+    VERIFY_EQUAL(pt3a, pt3d);
+
+    Math::Point4i pt4a(123, 123, 123, 123);
+    Math::Point4i pt4b(124, 124, 124, 124);
+    Math::Vector4i vec4 = pt4b - pt4a;
+    VERIFY_EQUAL(1, vec4.x);
+    VERIFY_EQUAL(1, vec4.y);
+    VERIFY_EQUAL(1, vec4.z);
+    VERIFY_EQUAL(1, vec4.w);
+
+    Math::Point4i pt4c = pt4a + vec4;
+    VERIFY_EQUAL(pt4b, pt4c);
     
-    VERIFY_EQUAL(pt1, pt4);
+    Math::Point4i pt4d = pt4b - vec4;
+    VERIFY_EQUAL(pt4a, pt4d);
 }
 
 SCENARIO( Point, "Operation/Normal", "unit,math", "Points can convert to vector." )
 {
-    Math::Point2i pt1(123, 123);
+    Math::Point1i pt1(123);
+    Math::Vector1i vec1 = pt1;
     
-    Math::Vector2i diff = pt1;
+    VERIFY_EQUAL(123, vec1.x);
+
+    Math::Point2i pt2(123, 123);
+    Math::Vector2i vec2 = pt2;
     
-    VERIFY_EQUAL(123, diff.x);
-    VERIFY_EQUAL(123, diff.y);
+    VERIFY_EQUAL(123, vec2.x);
+    VERIFY_EQUAL(123, vec2.y);
+
+    Math::Point3i pt3(123, 123, 123);
+    Math::Vector3i vec3 = pt3;
+    
+    VERIFY_EQUAL(123, vec3.x);
+    VERIFY_EQUAL(123, vec3.y);
+    VERIFY_EQUAL(123, vec3.z);
+
+    Math::Point4i pt4(123, 123, 123, 123);
+    Math::Vector4i vec4 = pt4;
+    
+    VERIFY_EQUAL(123, vec4.x);
+    VERIFY_EQUAL(123, vec4.y);
+    VERIFY_EQUAL(123, vec4.z);
+    VERIFY_EQUAL(123, vec4.w);
 }
--- Math/Tests/VectorScenarios.cpp
+++ Math/Tests/VectorScenarios.cpp
@@ -15,135 +15,294 @@ using namespace TUCUT;
 
 SCENARIO( Vector, "Construction/Normal", "unit,math", "Vectors can be constructed." )
 {
-    Math::Vector1i vec1;
-    Math::Vector2i vec2;
-    Math::Vector3i vec3;
-    Math::Vector4i vec4;
-    Math::Vector1i vec5(123);
-    Math::Vector2i vec6(123, 124);
-    Math::Vector3i vec7(123, 124, 125);
-    Math::Vector4i vec8(123, 124, 125, 126);
+    Math::Vector1i vec1a;
+    Math::Vector2i vec2a;
+    Math::Vector3i vec3a;
+    Math::Vector4i vec4a;
+    Math::Vector1i vec1b(123);
+    Math::Vector2i vec2b(123, 124);
+    Math::Vector3i vec3b(123, 124, 125);
+    Math::Vector4i vec4b(123, 124, 125, 126);
     
-    VERIFY_EQUAL(0, vec1.x);
+    VERIFY_EQUAL(0, vec1a.x);
     
-    VERIFY_EQUAL(0, vec2.x);
-    VERIFY_EQUAL(0, vec2.y);
+    VERIFY_EQUAL(0, vec2a.x);
+    VERIFY_EQUAL(0, vec2a.y);
     
-    VERIFY_EQUAL(0, vec3.x);
-    VERIFY_EQUAL(0, vec3.y);
-    VERIFY_EQUAL(0, vec3.z);
+    VERIFY_EQUAL(0, vec3a.x);
+    VERIFY_EQUAL(0, vec3a.y);
+    VERIFY_EQUAL(0, vec3a.z);
     
-    VERIFY_EQUAL(0, vec4.x);
-    VERIFY_EQUAL(0, vec4.y);
-    VERIFY_EQUAL(0, vec4.z);
-    VERIFY_EQUAL(0, vec4.w);
+    VERIFY_EQUAL(0, vec4a.x);
+    VERIFY_EQUAL(0, vec4a.y);
+    VERIFY_EQUAL(0, vec4a.z);
+    VERIFY_EQUAL(0, vec4a.w);
     
-    VERIFY_EQUAL(123, vec5.x);
+    VERIFY_EQUAL(123, vec1b.x);
     
-    VERIFY_EQUAL(123, vec6.x);
-    VERIFY_EQUAL(124, vec6.y);
+    VERIFY_EQUAL(123, vec2b.x);
+    VERIFY_EQUAL(124, vec2b.y);
     
-    VERIFY_EQUAL(123, vec7.x);
-    VERIFY_EQUAL(124, vec7.y);
-    VERIFY_EQUAL(125, vec7.z);
+    VERIFY_EQUAL(123, vec3b.x);
+    VERIFY_EQUAL(124, vec3b.y);
+    VERIFY_EQUAL(125, vec3b.z);
     
-    VERIFY_EQUAL(123, vec8.x);
-    VERIFY_EQUAL(124, vec8.y);
-    VERIFY_EQUAL(125, vec8.z);
-    VERIFY_EQUAL(126, vec8.w);
+    VERIFY_EQUAL(123, vec4b.x);
+    VERIFY_EQUAL(124, vec4b.y);
+    VERIFY_EQUAL(125, vec4b.z);
+    VERIFY_EQUAL(126, vec4b.w);
 }
 
 SCENARIO( Vector, "Construction/Normal", "unit,math", "Vectors can be copy constructed." )
 {
-    Math::Vector1i vec1(123);
-    Math::Vector1i vec2(vec1);
+    Math::Vector1i vec1a(123);
+    Math::Vector1i vec1b(vec1a);
     
-    VERIFY_EQUAL(vec1, vec2);
+    VERIFY_EQUAL(vec1a, vec1b);
+    
+    Math::Vector2i vec2a(123, 124);
+    Math::Vector2i vec2b(vec2a);
+    
+    VERIFY_EQUAL(vec2a, vec2b);
+    
+    Math::Vector3i vec3a(123, 124, 125);
+    Math::Vector3i vec3b(vec3a);
+    
+    VERIFY_EQUAL(vec3a, vec3b);
+    
+    Math::Vector4i vec4a(123, 124, 125, 126);
+    Math::Vector4i vec4b(vec4a);
+    
+    VERIFY_EQUAL(vec4a, vec4b);
 }
 
 SCENARIO( Vector, "Construction/Normal", "unit,math", "Vectors can be unit constructed." )
 {
-    Math::Vector3i vec1 = Math::Vector3i::one();
-    Math::Vector4d vec2 = Math::Vector4d::one();
+    Math::Vector1i vec1a = Math::Vector1i::one();
+    Math::Vector1d vec1b = Math::Vector1d::one();
     
-    VERIFY_EQUAL(1, vec1.x);
-    VERIFY_EQUAL(1, vec1.y);
-    VERIFY_EQUAL(1, vec1.z);
+    VERIFY_EQUAL(1, vec1a.x);
+    VERIFY_EQUAL(1.0, vec1b.x);
+
+    Math::Vector2i vec2a = Math::Vector2i::one();
+    Math::Vector2d vec2b = Math::Vector2d::one();
     
-    VERIFY_EQUAL(1.0, vec2.x);
-    VERIFY_EQUAL(1.0, vec2.y);
-    VERIFY_EQUAL(1.0, vec2.z);
-    VERIFY_EQUAL(1.0, vec2.w);
+    VERIFY_EQUAL(1, vec2a.x);
+    VERIFY_EQUAL(1, vec2a.y);
+    VERIFY_EQUAL(1.0, vec2b.x);
+    VERIFY_EQUAL(1.0, vec2b.y);
+
+    Math::Vector3i vec3a = Math::Vector3i::one();
+    Math::Vector3d vec3b = Math::Vector3d::one();
+    
+    VERIFY_EQUAL(1, vec3a.x);
+    VERIFY_EQUAL(1, vec3a.y);
+    VERIFY_EQUAL(1, vec3a.z);
+    VERIFY_EQUAL(1.0, vec3b.x);
+    VERIFY_EQUAL(1.0, vec3b.y);
+    VERIFY_EQUAL(1.0, vec3b.z);
+
+    Math::Vector4i vec4a = Math::Vector4i::one();
+    Math::Vector4d vec4b = Math::Vector4d::one();
+    
+    VERIFY_EQUAL(1, vec4a.x);
+    VERIFY_EQUAL(1, vec4a.y);
+    VERIFY_EQUAL(1, vec4a.z);
+    VERIFY_EQUAL(1, vec4a.w);
+    VERIFY_EQUAL(1.0, vec4b.x);
+    VERIFY_EQUAL(1.0, vec4b.y);
+    VERIFY_EQUAL(1.0, vec4b.z);
+    VERIFY_EQUAL(1.0, vec4b.w);
 }
 
 SCENARIO( Vector, "Operation/Normal", "unit,math", "Vectors can be assigned." )
 {
-    Math::Vector1i vec1(123);
-    Math::Vector1i vec2(124);
+    Math::Vector1i vec1a(123);
+    Math::Vector1i vec1b(124);
     
-    VERIFY_FALSE(vec1 == vec2);
-    vec2 = vec1;
-    VERIFY_EQUAL(vec1, vec2);
+    VERIFY_FALSE(vec1a == vec1b);
+    vec1b = vec1a;
+    VERIFY_EQUAL(vec1a, vec1b);
+    
+    Math::Vector2i vec2a(123, 123);
+    Math::Vector2i vec2b(124, 124);
+    
+    VERIFY_FALSE(vec2a == vec2b);
+    vec2b = vec2a;
+    VERIFY_EQUAL(vec2a, vec2b);
+    
+    Math::Vector3i vec3a(123, 123, 123);
+    Math::Vector3i vec3b(124, 124, 124);
+    
+    VERIFY_FALSE(vec3a == vec3b);
+    vec3b = vec3a;
+    VERIFY_EQUAL(vec3a, vec3b);
+    
+    Math::Vector4i vec4a(123, 123, 123, 123);
+    Math::Vector4i vec4b(124, 124, 124, 124);
+    
+    VERIFY_FALSE(vec4a == vec4b);
+    vec4b = vec4a;
+    VERIFY_EQUAL(vec4a, vec4b);
 }
 
 SCENARIO( Vector, "Operation/Normal", "unit,math", "Vectors can be added." )
 {
-    Math::Vector2i vec1(1, 2);
-    Math::Vector2i vec2(4, 5);
+    Math::Vector1i vec1a(1);
+    Math::Vector1i vec1b(4);
     
-    Math::Vector2i sum = vec1 + vec2;
+    Math::Vector1i sum1 = vec1a + vec1b;
+    VERIFY_EQUAL(5, sum1.x);
+
+    Math::Vector2i vec2a(1, 2);
+    Math::Vector2i vec2b(4, 5);
     
-    VERIFY_EQUAL(5, sum.x);
-    VERIFY_EQUAL(7, sum.y);
+    Math::Vector2i sum2 = vec2a + vec2b;
+    VERIFY_EQUAL(5, sum2.x);
+    VERIFY_EQUAL(7, sum2.y);
+
+    Math::Vector3i vec3a(1, 2, 3);
+    Math::Vector3i vec3b(4, 5, 6);
+    
+    Math::Vector3i sum3 = vec3a + vec3b;
+    VERIFY_EQUAL(5, sum3.x);
+    VERIFY_EQUAL(7, sum3.y);
+    VERIFY_EQUAL(9, sum3.z);
+
+    Math::Vector4i vec4a(1, 2, 3, 4);
+    Math::Vector4i vec4b(4, 5, 6, 7);
+    
+    Math::Vector4i sum4 = vec4a + vec4b;
+    VERIFY_EQUAL(5, sum4.x);
+    VERIFY_EQUAL(7, sum4.y);
+    VERIFY_EQUAL(9, sum4.z);
+    VERIFY_EQUAL(11, sum4.w);
 }
 
 SCENARIO( Vector, "Operation/Normal", "unit,math", "Vectors can be subtracted." )
 {
-    Math::Vector2i vec1(1, 2);
-    Math::Vector2i vec2(4, 5);
+    Math::Vector1i vec1a(1);
+    Math::Vector1i vec1b(4);
     
-    Math::Vector2i diff = vec2 - vec1;
+    Math::Vector1i diff1 = vec1b - vec1a;
+    VERIFY_EQUAL(3, diff1.x);
     
-    VERIFY_EQUAL(3, diff.x);
-    VERIFY_EQUAL(3, diff.y);
+    Math::Vector2i vec2a(1, 2);
+    Math::Vector2i vec2b(4, 5);
+    
+    Math::Vector2i diff2 = vec2b - vec2a;
+    VERIFY_EQUAL(3, diff2.x);
+    VERIFY_EQUAL(3, diff2.y);
+    
+    Math::Vector3i vec3a(1, 2, 3);
+    Math::Vector3i vec3b(4, 5, 6);
+    
+    Math::Vector3i diff3 = vec3b - vec3a;
+    VERIFY_EQUAL(3, diff3.x);
+    VERIFY_EQUAL(3, diff3.y);
+    VERIFY_EQUAL(3, diff3.z);
+    
+    Math::Vector4i vec4a(1, 2, 3, 4);
+    Math::Vector4i vec4b(4, 5, 6, 7);
+    
+    Math::Vector4i diff4 = vec4b - vec4a;
+    VERIFY_EQUAL(3, diff4.x);
+    VERIFY_EQUAL(3, diff4.y);
+    VERIFY_EQUAL(3, diff4.z);
+    VERIFY_EQUAL(3, diff4.w);
 }
 
 SCENARIO( Vector, "Operation/Normal", "unit,math", "Vectors can be multiplied." )
 {
-    Math::Vector2i vec1(2, 3);
+    Math::Vector1i vec1(2);
     
-    Math::Vector2i prod = vec1 * 3;
+    Math::Vector1i prod1 = vec1 * 3;
+    VERIFY_EQUAL(6, prod1.x);
+
+    Math::Vector2i vec2(2, 3);
     
-    VERIFY_EQUAL(6, prod.x);
-    VERIFY_EQUAL(9, prod.y);
+    Math::Vector2i prod2 = vec2 * 3;
+    VERIFY_EQUAL(6, prod2.x);
+    VERIFY_EQUAL(9, prod2.y);
+
+    Math::Vector3i vec3(2, 3, 4);
+    
+    Math::Vector3i prod3 = vec3 * 3;
+    VERIFY_EQUAL(6, prod3.x);
+    VERIFY_EQUAL(9, prod3.y);
+    VERIFY_EQUAL(12, prod3.z);
+
+    Math::Vector4i vec4(2, 3, 4, 5);
+    
+    Math::Vector4i prod4 = vec4 * 3;
+    VERIFY_EQUAL(6, prod4.x);
+    VERIFY_EQUAL(9, prod4.y);
+    VERIFY_EQUAL(12, prod4.z);
+    VERIFY_EQUAL(15, prod4.w);
 }
 
 SCENARIO( Vector, "Operation/Normal", "unit,math", "Vectors can be divided." )
 {
-    Math::Vector2i vec1(2, 10);
+    Math::Vector1i vec1(2);
     
-    Math::Vector2i quot = vec1 / 2;
+    Math::Vector1i quot1 = vec1 / 2;
+    VERIFY_EQUAL(1, quot1.x);
+
+    Math::Vector2i vec2(2, 10);
     
-    VERIFY_EQUAL(1, quot.x);
-    VERIFY_EQUAL(5, quot.y);
+    Math::Vector2i quot2 = vec2 / 2;
+    VERIFY_EQUAL(1, quot2.x);
+    VERIFY_EQUAL(5, quot2.y);
+
+    Math::Vector3i vec3(2, 10, 14);
+    
+    Math::Vector3i quot3 = vec3 / 2;
+    VERIFY_EQUAL(1, quot3.x);
+    VERIFY_EQUAL(5, quot3.y);
+    VERIFY_EQUAL(7, quot3.z);
+
+    Math::Vector4i vec4(2, 10, 14, 18);
+    
+    Math::Vector4i quot4 = vec4 / 2;
+    VERIFY_EQUAL(1, quot4.x);
+    VERIFY_EQUAL(5, quot4.y);
+    VERIFY_EQUAL(7, quot4.z);
+    VERIFY_EQUAL(9, quot4.w);
 }
 
 SCENARIO( Vector, "Operation/Normal", "unit,math", "Vectors can be dot multiplied." )
 {
-    Math::Vector2i vec1(2, 3);
-    Math::Vector2i vec2(5, 7);
-
-    int dot = vec1.dot(vec2);
+    Math::Vector1i vec1a(2);
+    Math::Vector1i vec1b(5);
     
-    VERIFY_EQUAL(31, dot);
+    int dot1 = vec1a.dot(vec1b);
+    VERIFY_EQUAL(10, dot1);
+
+    Math::Vector2i vec2a(2, 3);
+    Math::Vector2i vec2b(5, 7);
+
+    int dot2 = vec2a.dot(vec2b);
+    VERIFY_EQUAL(31, dot2);
+
+    Math::Vector3i vec3a(2, 3, 4);
+    Math::Vector3i vec3b(5, 7, 9);
+    
+    int dot3 = vec3a.dot(vec3b);
+    VERIFY_EQUAL(67, dot3);
+
+    Math::Vector4i vec4a(2, 3, 4, 5);
+    Math::Vector4i vec4b(5, 7, 9, 11);
+    
+    int dot4 = vec4a.dot(vec4b);
+    VERIFY_EQUAL(122, dot4);
 }
 
 SCENARIO( Vector, "Operation/Normal", "unit,math", "Vector3s can be cross multiplied." )
 {
-    Math::Vector3i vec1(2, 10, 5);
-    Math::Vector3i vec2(-1, 4, 0);
+    Math::Vector3i vec3a(2, 10, 5);
+    Math::Vector3i vec3b(-1, 4, 0);
     
-    // vec1  vec2
+    // vec3a vec3b
     //   2    -1
     //  10     4
     //   5     0
@@ -153,9 +312,9 @@ SCENARIO( Vector, "Operation/Normal", "unit,math", "Vector3s can be cross multip
     //   5 * -1 - 2 * 0   =  -5
     //   2 * 4  - 10 * -1 =  18
 
-    Math::Vector3i prod = vec1.cross(vec2);
+    Math::Vector3i prod3 = vec3a.cross(vec3b);
     
-    VERIFY_EQUAL(-20, prod.x);
-    VERIFY_EQUAL(-5, prod.y);
-    VERIFY_EQUAL(18, prod.z);
+    VERIFY_EQUAL(-20, prod3.x);
+    VERIFY_EQUAL(-5, prod3.y);
+    VERIFY_EQUAL(18, prod3.z);
 }
